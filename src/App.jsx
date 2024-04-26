773155c42dae41eb0cfd2ee59eebff1665c9905b
nicocaputocai <nicolas_caputo@hotmail.com>
2024-04-25 21:38:28 -0300

    primer push

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Banner } from './Components/Banner'
+import { Description } from './Components/Description'
+
+function App() {
+
+
+  return (
+    <>
+     <Banner />
+     <Description />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Description.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import ".././App.css";
+
+export const Description = () => {
+  return (
+    <Container>
+<div className="text-container">
+        <p className="main-text">
+          En <span className="verdeManzana">ECO global</span> brindamos soluciones ambientales innovadoras. Impulsamos un cambio real y duradero hacia prácticas empresariales y gubernamentales más sostenibles desde un enfoque público y privado.
+        </p>
+      </div>
+    </Container>
+  );
+};
